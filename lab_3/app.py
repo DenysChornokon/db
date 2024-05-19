8d1b337f4772b4c33314f427a7dadba5c88db093
Denys Chornokon <denyschornokon3@gmail.com>
2024-05-19 14:49:02 +0300

    lab 3 done

--- lab_3/app.py
+++ lab_3/app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, jsonify
 import psycopg2
+import psycopg2.extras
 
 app = Flask(__name__)
 
-# Підключення до PostgreSQL 'postgresql://postgres:admin@127.0.0.1:5432/postgres'
+# Підключення до PostgreSQL
 conn = psycopg2.connect(
     dbname="library",
     user="postgres",
     password="admin",
-    host="localhost"
+    host="127.0.0.1",
+    options="-c client_encoding=UTF8"
 )
-cur = conn.cursor()
-
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # Створення нового читача
+
+
 @app.route('/reader', methods=['POST'])
 def create_reader():
     data = request.json
@@ -24,47 +27,53 @@ def create_reader():
     conn.commit()
     return jsonify({"message": "Reader created successfully"}), 201
 
-
 # Отримання інформації про всіх читачів
+
+
 @app.route('/reader', methods=['GET'])
 def get_all_readers():
     cur.execute("SELECT * FROM readers")
     readers = cur.fetchall()
-    return jsonify(readers)
-
+    return jsonify([dict(reader) for reader in readers])
 
 # Отримання інформації про конкретного читача за його ідентифікатором
+
+
 @app.route('/reader/<int:reader_id>', methods=['GET'])
 def get_reader_by_id(reader_id):
     cur.execute("SELECT * FROM readers WHERE id = %s", (reader_id,))
     reader = cur.fetchone()
     if reader:
-        return jsonify(reader)
+        return jsonify(dict(reader))
     else:
         return jsonify({"message": "Reader not found"}), 404
 
-
 # Оновлення інформації про читача
+
+
 @app.route('/reader/<int:reader_id>', methods=['PUT'])
 def update_reader(reader_id):
     data = request.json
     name = data['name']
     address = data['address']
     phone_number = data['phone_number']
-    cur.execute("CALL update_reader(%s, %s, %s, %s)", (reader_id, name, address, phone_number))
+    cur.execute("CALL update_reader(%s, %s, %s, %s)",
+                (reader_id, name, address, phone_number))
     conn.commit()
     return jsonify({"message": "Reader updated successfully"})
 
-
 # Видалення читача за його ідентифікатором
+
+
 @app.route('/reader/<int:reader_id>', methods=['DELETE'])
 def delete_reader(reader_id):
     cur.execute("DELETE FROM readers WHERE id = %s", (reader_id,))
     conn.commit()
     return jsonify({"message": "Reader deleted successfully"})
 
-
 # Створення нової книги
+
+
 @app.route('/book', methods=['POST'])
 def create_book():
     data = request.json
@@ -73,31 +82,35 @@ def create_book():
     genre = data['genre']
     publication_year = data['publication_year']
     pages = data['pages']
-    cur.execute("CALL add_book(%s, %s, %s, %s, %s)", (title, author, genre, publication_year, pages))
+    cur.execute("CALL add_book(%s, %s, %s, %s, %s)",
+                (title, author, genre, publication_year, pages))
     conn.commit()
     return jsonify({"message": "Book created successfully"}), 201
 
-
 # Отримання інформації про всі книги
+
+
 @app.route('/book', methods=['GET'])
 def get_all_books():
     cur.execute("SELECT * FROM books")
     books = cur.fetchall()
-    return jsonify(books)
-
+    return jsonify([dict(book) for book in books])
 
 # Отримання інформації про конкретну книгу за її ідентифікатором
+
+
 @app.route('/book/<int:book_id>', methods=['GET'])
 def get_book_by_id(book_id):
     cur.execute("SELECT * FROM books WHERE id = %s", (book_id,))
     book = cur.fetchone()
     if book:
-        return jsonify(book)
+        return jsonify(dict(book))
     else:
         return jsonify({"message": "Book not found"}), 404
 
-
 # Оновлення інформації про книгу
+
+
 @app.route('/book/<int:book_id>', methods=['PUT'])
 def update_book(book_id):
     data = request.json
@@ -111,16 +124,18 @@ def update_book(book_id):
     conn.commit()
     return jsonify({"message": "Book updated successfully"})
 
-
 # Видалення книги за її ідентифікатором
+
+
 @app.route('/book/<int:book_id>', methods=['DELETE'])
 def delete_book(book_id):
     cur.execute("DELETE FROM books WHERE id = %s", (book_id,))
     conn.commit()
     return jsonify({"message": "Book deleted successfully"})
 
-
 # Створення нової позики
+
+
 @app.route('/loan', methods=['POST'])
 def create_loan():
     data = request.json
@@ -133,27 +148,30 @@ def create_loan():
     conn.commit()
     return jsonify({"message": "Loan created successfully"}), 201
 
-
 # Отримання інформації про всі позики
+
+
 @app.route('/loan', methods=['GET'])
 def get_all_loans():
     cur.execute("SELECT * FROM loans")
     loans = cur.fetchall()
-    return jsonify(loans)
-
+    return jsonify([dict(loan) for loan in loans])
 
 # Отримання інформації про конкретну позику за її ідентифікатором
+
+
 @app.route('/loan/<int:loan_id>', methods=['GET'])
 def get_loan_by_id(loan_id):
     cur.execute("SELECT * FROM loans WHERE id = %s", (loan_id,))
     loan = cur.fetchone()
     if loan:
-        return jsonify(loan)
+        return jsonify(dict(loan))
     else:
         return jsonify({"message": "Loan not found"}), 404
 
-
 # Оновлення інформації про позику
+
+
 @app.route('/loan/<int:loan_id>', methods=['PUT'])
 def update_loan(loan_id):
     data = request.json
@@ -166,8 +184,9 @@ def update_loan(loan_id):
     conn.commit()
     return jsonify({"message": "Loan updated successfully"})
 
-
 # Видалення позики за її ідентифікатором
+
+
 @app.route('/loan/<int:loan_id>', methods=['DELETE'])
 def delete_loan(loan_id):
     cur.execute("CALL delete_loan(%s)", (loan_id,))
